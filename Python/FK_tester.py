de71bebd625c4b512a7095bea811dbaf8f253b6f
meldew <kirillgja@gmail.com>
2023-07-15 13:44:59 +0200

    FK teter fix

--- Python/FK_tester.py
+++ Python/FK_tester.py
@@ -1,12 +1,12 @@
 import forward_kinematics as FK
 
-J1 , J2, J3, J4, J5, J6 = FK.calculate_forward_kinematics(60,60,60,60,60,60)
+Xpos , Ypos, Zpos, roll, pitch, yaw = FK.calculate_forward_kinematics(60,60,60,60,60,60)
 
 print("..................................")
-print("J1 = ", J1)
-print("J2 = ", J2)
-print("J3 = ", J3)
-print("J4 = ", J4)
-print("J5 = ", J5)
-print("J6 = ", J6)
+print("Xpos = ", Xpos)
+print("Ypos = ", Ypos)
+print("Zpos = ", Zpos)
+print("roll = ", roll)
+print("pitch = ", pitch)
+print("yow = ", yaw)
 print("..................................")
