7e6aaa01d005e32447f1b7dc304adfa09c56bab3
meldew <kirillgja@gmail.com>
2023-07-17 22:18:49 +0200

    Invese kinematics done and tested

--- Python/FK_and_IK_tester.py
+++ Python/FK_and_IK_tester.py
@@ -1,39 +1,28 @@
 import forward_kinematics as FK
-import numpy as np
-import math as m
-Xpos , Ypos, Zpos, roll, pitch, yaw = FK.calculate_forward_kinematics(60,60,60,60,60,60)
+import inverse_kinematics as IK
 
-#print("..................................")
-#print("Xpos = ", Xpos)
-#print("Ypos = ", Ypos)
-#rint("Zpos = ", Zpos)
-#print("roll = ", roll)
-#print("pitch = ", pitch)
-#print("yow = ", yaw)
-#print("..................................")
+J1 = 0.01
+J2 = -90
+J3 = 90
+J4 = 0.01
+J5 = 0.01
+J6 = 0.01
 
-def Overall_tool_translation_matrix(x, y, z, yaw, pitch, roll):
-    alpha = np.deg2rad(yaw)
-    beta = np.deg2rad(pitch)
-    gamma = np.deg2rad(roll)
 
-    T_0_T = np.array([[(np.cos(alpha)*np.cos(gamma))-(np.cos(beta)*np.sin(alpha)*np.sin(gamma)), (np.cos(gamma)*np.sin(alpha))+(np.cos(alpha)*np.cos(beta)*np.sin(gamma)), np.sin(beta)*np.sin(gamma), x],
-                      [(np.cos(beta)*np.cos(gamma)*np.sin(alpha))+(np.cos(alpha)*np.sin(gamma)), (np.cos(alpha)*np.cos(beta)*np.cos(gamma))-(np.sin(alpha)*np.sin(gamma)), np.cos(gamma)*np.sin(beta), y],
-                      [np.sin(alpha)*np.sin(beta), np.cos(alpha)*np.sin(beta), -np.cos(beta), z],
-                        [0, 0, 0, 1]])
-    value = np.cos(gamma)*np.sin(beta)
-   
-   
-    
-    return T_0_T
-
-print(Overall_tool_translation_matrix(64.9094406653893, 58.0514491233303, -295.019339020579, 176.329503491685, 102.503916617342, -20.1944289077349))
-T_0_T = Overall_tool_translation_matrix(64.9094406653893, 58.0514491233303, -295.019339020579, 176.329503491685, 102.503916617342, -20.1944289077349)
-work_frame = np.array([[1, 0, 0, 0],
-                       [0, 1, 0, 0],
-                       [0, 0, 1, 0],
-                       [0, 0, 0, 1]])
-
-T_0_T_offset = np.dot(T_0_T, work_frame)
-T_0_T_offset[0,0] = T_0_T_offset[0,0]*-1
-print(T_0_T_offset)
+Xpos, Ypos, Zpos, roll, pitch, yaw = FK.calculate_forward_kinematics(J1, J2, J3, J4, J5, J6)
+J1new, J2new, J3new, J4new, J5new, J6new = IK.calculate_inverse_kinematics(Xpos, Ypos, Zpos, yaw, pitch, roll, J1, J2, J3, J5)
+print("............................................")
+print("Xpos: ", Xpos)
+print("Ypos: ", Ypos)
+print("Zpos: ", Zpos)
+print("roll: ", roll)
+print("pitch: ", pitch)
+print("yaw: ", yaw)
+print("............................................")
+print("J1new: ", J1new)
+print("J2new: ", J2new)
+print("J3new: ", J3new)
+print("J4new: ", J4new)
+print("J5new: ", J5new)
+print("J6new: ", J6new)
+print("............................................")
--- Python/inverse_kinematics.py
+++ Python/inverse_kinematics.py
@@ -19,13 +19,13 @@ def Overall_tool_translation_matrix(x, y, z, yaw, pitch, roll):
     return T_0_T
                        
 # Function to calculate inverse kinematics
-def calculate_inverse_kinematics(x, y, z, yaw, pitch, roll, Feedback_Joint_angle1, Feedback_Joint_angle2, Feedback_Joint_angle3):
+def calculate_inverse_kinematics(x, y, z, yaw, pitch, roll, Feedback_Joint_angle1, Feedback_Joint_angle2, Feedback_Joint_angle3, Feedback_Joint_angle5):
 
     # Calculate the Rotation matrix from the base frame to the J3 frame and then take an inverse of it.
     DH = DH_parameters()
     Theta1 = np.deg2rad(Feedback_Joint_angle1)
     Theta2 = np.deg2rad(Feedback_Joint_angle2)
-    Theta3 = np.deg2rad(Feedback_Joint_angle3)
+    Theta3 = np.deg2rad(Feedback_Joint_angle3 - 90 )
 
     J1 = DH_matrix(Theta1, DH.d1, DH.a1, DH.alpha1)   
     J2 = DH_matrix(Theta2, DH.d2, DH.a2, DH.alpha2)
@@ -59,4 +59,77 @@ def calculate_inverse_kinematics(x, y, z, yaw, pitch, roll, Feedback_Joint_angle
                                     [center_spherical_wrist[2,0], center_spherical_wrist[2,1], center_spherical_wrist[2,2]]])
     
     # Now we mathematically connect previously decoupled 0 to 3 rotation matrix to the 3 to 6 rotation matrix
-    R_3_6_orientation_matrix = R_0_3_Transposed.dot(central_wrist_Rot)
+    R_3_6_orientation_matrix = np.dot(R_0_3_Transposed,central_wrist_Rot)
+
+    # Fet J4 to J6 angles from the connected rotation matrix
+    J5_pos = np.rad2deg(np.arctan2(np.sqrt(1- np.power(R_3_6_orientation_matrix[2, 2],2)), R_3_6_orientation_matrix[2, 2]))
+    J5_neg = np.rad2deg(np.arctan2(-np.sqrt(1- np.power(R_3_6_orientation_matrix[2, 2],2)), R_3_6_orientation_matrix[2, 2]))
+
+    if J5_pos > 0 and Feedback_Joint_angle5 > 0: J5 = J5_pos
+    else: J5 = J5_neg
+
+    if R_3_6_orientation_matrix[2, 1] < 0: J6_pos = np.rad2deg(np.arctan2(-R_3_6_orientation_matrix[2, 1], R_3_6_orientation_matrix[2, 0])) - 180
+    else: J6_pos = np.rad2deg(np.arctan2(-R_3_6_orientation_matrix[2, 1], R_3_6_orientation_matrix[2, 0])) + 180
+
+    if R_3_6_orientation_matrix[2, 1] < 0: J6_neg = np.rad2deg(np.arctan2(R_3_6_orientation_matrix[2, 1], -R_3_6_orientation_matrix[2, 0])) + 180
+    else: J6_neg = np.rad2deg(np.arctan2(R_3_6_orientation_matrix[2, 1], -R_3_6_orientation_matrix[2, 0])) - 180
+
+    if J5 < 0: J6 = J6_neg
+    else: J6 = J6_pos
+
+    J4_pos = np.rad2deg(np.arctan2(R_3_6_orientation_matrix[1, 2], R_3_6_orientation_matrix[0, 2]))
+    J4_neg = np.rad2deg(np.arctan2(-R_3_6_orientation_matrix[1, 2], -R_3_6_orientation_matrix[0, 2]))
+
+    if J5 > 0: J4 = J4_pos
+    else: J4 = J4_neg
+
+    if x > 0 and y > 0: quadrant = 1
+    elif x > 0 and y < 0: quadrant = 2 
+    elif x < 0 and y < 0: quadrant = 3
+    elif x < 0 and y > 0: quadrant = 4
+
+    # Use geometry to find J1, J2, J3 angles
+    J1_rad = np.arctan(center_spherical_wrist[1, 3]/center_spherical_wrist[0, 3])
+    if quadrant == 1: J1 = np.rad2deg(J1_rad)
+    elif quadrant == 2: J1 = np.rad2deg(J1_rad)
+    elif quadrant == 3: J1 = np.rad2deg(J1_rad) + (-180)
+    elif quadrant == 4: J1 = np.rad2deg(J1_rad) + 180
+
+    pX = np.sqrt(np.power(np.abs(center_spherical_wrist[0, 3]),2)+np.power(np.abs(center_spherical_wrist[1, 3]),2))
+    pY = center_spherical_wrist[2, 3] - DH.d1
+    pX_a1_FWD = pX - DH.a1
+    pX_a1_MID = -pX_a1_FWD
+    pa2H_FWD = np.sqrt(np.power(pY,2) + np.power(pX_a1_FWD,2))
+    pa2H_MID = np.sqrt(np.power(pY,2) + np.power(pX_a1_MID,2))
+    pa3H = np.sqrt(np.power(DH.d4,2) + np.power(DH.a3,2))
+
+    thetaA_FWD = np.rad2deg(np.arctan(pY/pX_a1_FWD))
+    thetaA_MID = np.rad2deg(np.arccos((np.power(DH.a2,2)+np.power(pa2H_MID,2)-np.power(np.abs(DH.d4),2))/(2*DH.a2*pa2H_MID)))
+
+    thetaB_FWD = np.rad2deg(np.arccos((np.power(DH.a2,2) + np.power(pa2H_FWD,2) - (np.power(np.abs(pa3H),2))) / (2*DH.a2*pa2H_FWD)))
+    thetaB_MID = np.rad2deg(np.arctan(pX_a1_MID/pY))
+
+    thetaD = 90 - (thetaA_MID + thetaB_MID)
+    try:
+        thetaE = np.rad2deg(np.arctan(np.abs(DH.d4)/DH.a3))
+    except:
+        thetaE = 90
+
+    thetaC_FWD = 180 - np.rad2deg(np.arccos((np.power(np.abs(pa3H),2)+np.power(DH.a2,2)-np.power(pa2H_FWD,2))/(2*np.abs(pa3H)*DH.a2))) + (90 - thetaE)
+    thetaC_MID = 180 - np.rad2deg(np.arccos((np.power(np.abs(pa3H),2)+np.power(DH.a2,2)-np.power(pa2H_MID,2))/(2*np.abs(pa3H)*DH.a2))) + (90 - thetaE)
+
+    J2_FWD = -(thetaA_FWD + thetaB_FWD)
+    J2_MID = -180 + thetaD
+
+    J3_FWD = thetaC_FWD
+    J3_MID = thetaC_MID
+
+    if pX_a1_FWD < 0: J2 = J2_MID
+    else: J2 = J2_FWD
+
+    if pX_a1_FWD < 0: J3 = J3_MID
+    else: J3 = J3_FWD
+
+    print(J4_pos, J4_neg, J5_pos, J5_neg, J6_pos, J6_neg)
+
+    return J1, J2, J3, J4, J5, J6
