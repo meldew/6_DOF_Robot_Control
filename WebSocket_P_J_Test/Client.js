ae3c146428c06016fab682a0a31e6faff9591b6c
meldew <kirillgja@gmail.com>
2023-12-30 22:55:18 +0100

    meh, worki

--- WebSocket_P_J_Test/Client.js
+++ WebSocket_P_J_Test/Client.js
@@ -1,6 +1,5 @@
 
 const WebSocket = require('ws');
-
 const socket = new WebSocket('ws://127.0.0.1:8765');
 
 socket.on('open', () => {
@@ -11,11 +10,20 @@ socket.on('open', () => {
 console.log('WebSocket client created');
 socket.on('message', (data) => {
     console.log(`WebSocket message received: ${data}`);
-    module.exports = {data};
+    const parsedData = JSON.parse(data);
+    const angles = Object.values(parsedData)[0];
+    const linkAngles = angles.map((angle, index) => {
+        if (index === 0) {
+            return angle !== undefined ? angle : 0; 
+        }
+        return isNaN(angle) ? 0 : angle;
+    });
+    module.exports.linkAngles = linkAngles;
     const sensorData = { message: 'Not for now data from JavaScript'};
     socket.send(JSON.stringify(sensorData));
-    
+    console.log(linkAngles);
 });
+
 socket.on('close', () => {
     console.log('WebSocket connection closed');
 });
