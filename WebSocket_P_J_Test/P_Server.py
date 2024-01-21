79ba8b01f65ca72709c9bbf6b2d239841038883e
Mikael Walde <kirillgja@gmail.com>
2024-01-21 15:53:02 +0100

    fix

--- WebSocket_P_J_Test/P_Server.py
+++ WebSocket_P_J_Test/P_Server.py
@@ -28,39 +28,33 @@ async def send_values(websocket, path):
             await websocket.send(values_json) 
             
             if J1 >= 140:
-                J1direction = -1  # Start decrementing J1
+                J1direction = -1  
             elif J1 <= -140:
-                J1direction = 1  # Start incrementing J1
+                J1direction = 1  
 
             if J2 >= 100:
-                J2direction = -1  # Start decrementing J1
+                J2direction = -1  
             elif J2 <= -90:
-                J2direction = 1  # Start incrementing J1
+                J2direction = 1  
 
             if J3 >= 140:
-                J3direction = -1  # Start decrementing J1
+                J3direction = -1  
             elif J3 <= -140:
-                J3direction = 1  # Start incrementing J1
+                J3direction = 1  
 
             if J4 >= 180:
-                J4direction = -1  # Start decrementing J1
+                J4direction = -1  
             elif J4 <= -180:
-                J4direction = 1  # Start incrementing J1
+                J4direction = 1 
 
             J1 += J1direction  
             J2 += J2direction
             J3 += J3direction * 3
             J4 += J4direction * 3 
-
-                     
-            # Wait for a while before sending the values again
-            await asyncio.sleep(0.02)
-            
+            await asyncio.sleep(0.02)    
         except Exception as e:
             print(f"An error occurred: {e}")
             break
 start_server = websockets.serve(send_values, '127.0.0.1', 8765)
-
-
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
