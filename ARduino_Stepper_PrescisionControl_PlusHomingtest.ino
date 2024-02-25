03de95b4248f9fc21e0f41dcbadc50b7f89c08fb
Mikael <47281451+meldew@users.noreply.github.com>
2024-02-25 13:34:09 +0100

    Update ARduino_Stepper_PrescisionControl_PlusHomingtest.ino
    added m,ove to angle sequance

--- ARduino_Stepper_PrescisionControl_PlusHomingtest.ino
+++ ARduino_Stepper_PrescisionControl_PlusHomingtest.ino
@@ -3,12 +3,19 @@ const int dirPin = 3;
 const int limitSwitchPin = 2;
 const int limitSwitchPin2 = 5;
 const int homeSwitchPin = 6;
+const int moveTOButtonPin = 7; 
+const int resetHomingButtonPin = 8;
+
 const long stepsPerRevolution = 30620;
-const int stepsFor10Degrees = (stepsPerRevolution / 360);
-const int motorSpeed = 400;
+const int stepsFor1Degree = (stepsPerRevolution / 360);
+const int motorSpeed = 300;
+
 bool homingComplete = false;
 long currentPosition = 0;
 bool HomingRequest = HIGH;
+float targetAngle = false; 
+String inputString = "";
+bool stringComplete = false;
 
 void setup() {
   pinMode(stepPin, OUTPUT);
@@ -16,24 +23,62 @@ void setup() {
   pinMode(limitSwitchPin, INPUT_PULLUP);
   pinMode(limitSwitchPin2, INPUT_PULLUP);
   pinMode(homeSwitchPin, INPUT_PULLUP);
+  pinMode(moveTOButtonPin, INPUT_PULLUP);
+  pinMode(resetHomingButtonPin, INPUT_PULLUP);
   Serial.begin(9600);
 }
 
 void loop() {
+
+  if (digitalRead(resetHomingButtonPin) == LOW) {
+    while(digitalRead(resetHomingButtonPin) == LOW); 
+    homingComplete = false;
+    HomingRequest = HIGH;
+  }
+
+  if (stringComplete) {
+    targetAngle = inputString.toFloat(); 
+    Serial.print("Target Angle Set To: ");
+    Serial.println(targetAngle);
+    inputString = ""; 
+    stringComplete = false;
+  }
+
   if(HomingRequest == HIGH && homingComplete == false){
     performHoming();
   }
 
   if (homingComplete) {
     if (digitalRead(limitSwitchPin) == LOW) {
-      moveMotor(stepsFor10Degrees, HIGH);
+      moveMotor(stepsFor1Degree, HIGH);
     } else if (digitalRead(limitSwitchPin2) == LOW) {
-      moveMotor(stepsFor10Degrees, LOW);
+      moveMotor(stepsFor1Degree, LOW);
+    } else if (digitalRead(moveTOButtonPin) == LOW){
+      moveToAngle(targetAngle);
     }
     float currentAngle = calculateCurrentAngle();
     Serial.print("Current Angle: ");
     Serial.println(currentAngle, 2);
   }
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    inputString += inChar;
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
+}
+
+void moveToAngle (float angle){
+  long stepsTo_TargetPosition = angle * stepsPerRevolution / 360.0;
+  long stepsToMove = stepsTo_TargetPosition - currentPosition;
+  Serial.println(stepsTo_TargetPosition);
+  if (stepsToMove == 0) return; 
+  bool direction = stepsToMove > 0;
+  moveMotor(abs(stepsToMove), direction);
 }
 
 void performHoming() {
